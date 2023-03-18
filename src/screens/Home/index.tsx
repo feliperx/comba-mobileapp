2e2ac1b3087932a60f39be0d0808b86c2f3933fc
feliperx <feliipe.xaud@gmail.com>
2023-03-18 02:09:18 -0400

    add ListDivider component to Home flatlist

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, Image, StatusBar, TouchableOpacity, FlatList } from "react-native";
+import {
+  View,
+  FlatList,
+} from "react-native";
 
 import { styles } from "./styles";
 
@@ -9,73 +12,64 @@ import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategoryList } from "../../components/CategoryList";
 import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
 
 export function Home() {
+  const [category, setCategory] = useState("");
 
-    const [category, setCategory] = useState('');
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Legen.. wf ..dary",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "26/04 as 20h40",
+      description: "Dia de ganhar mais xp",
+    },
+    {
+      id: "2",
+      guild: {
+        id: "1",
+        name: "Legen.. wf ..dary",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "26/04 as 20h40",
+      description: "Dia de ganhar mais xp",
+    },
+  ];
 
-    const appointments = [
-        {
-            id: '1',
-            guild: {
-                id: '1',
-                name: 'Legen.. wf ..dary', 
-                icon: null,
-                owner: true
-            },
-            category: '1', 
-            date: '26/04 as 20h40', 
-            description: 'Dia de ganhar mais xp'
-        },
-        {
-            id: '2',
-            guild: {
-                id: '1',
-                name: 'Legen.. wf ..dary', 
-                icon: null,
-                owner: true
-            },
-            category: '1', 
-            date: '26/04 as 20h40', 
-            description: 'Dia de ganhar mais xp'
-        }
-    ]
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId);
+  }
 
-    function handleCategorySelect(categoryId: string) {
-        categoryId === category ? setCategory('') : setCategory(categoryId);
-    }
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}> 
-
-             <Avatar imageUrl="https://github.com/feliperx.png"/>  
-             <Profile/>
-             <ButtonAdd/>
-
-            </View>
-            <View>
-                <CategoryList
-                    categorySelected={category} 
-                    setCategory={handleCategorySelect}
-                />
-            </View>
-            <View style={styles.content}>
-                <ListHeader 
-                title='Partidas agendadas' 
-                subtitle='Total 7'
-                />
-                <FlatList
-                    data={appointments}
-                    keyExtractor={item => item.id}
-                    renderItem= {({item}) => (
-                        <Appointment data={item}/>
-                    )}
-                    style={styles.matches}
-                    showsVerticalScrollIndicator={false}
-                />
-            </View>
-        </View>
-    );
-    
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Avatar imageUrl="https://github.com/feliperx.png" />
+        <Profile />
+        <ButtonAdd />
+      </View>
+      <CategoryList
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="Total 7" />
+        <FlatList
+          data={appointments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
 }
