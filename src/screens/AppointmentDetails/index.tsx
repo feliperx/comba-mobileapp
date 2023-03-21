ada3d09f65bfc7974ff2a715bbeb020f07470d1d
feliperx <feliipe.xaud@gmail.com>
2023-03-20 22:43:05 -0400

    add AppointmentDetails screen

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { View, FlatList } from "react-native";
+
+import { styles } from "./styles";
+
+import { Profile } from "../../components/Profile";
+import { Avatar } from "../../components/Avatar";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategoryList } from "../../components/CategoryList";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
+import { Background } from "../../components/Background";
+
+export function AppointmentDetails() {
+  return (
+    <Background>
+      <View style={styles.container}>
+
+      </View>
+    </Background>
+  );
+}
