04f42e825e7dc8891fa111a1ae0660e355e25c18
feliperx <feliipe.xaud@gmail.com>
2023-03-23 16:03:43 -0400

    added SmallNumberInput component

---
+++ src/components/SmallNumberInput/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native"; 
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create ({
+  container: {
+    width: 48,
+    height: 48,
+    backgroundColor: theme.colors.secondary40,
+    borderRadius: 8, 
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.heading,
+    fontSize: 13, 
+    textAlign: 'center',
+    marginRight: 4
+
+  }
+
+});
