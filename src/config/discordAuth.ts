bb5164e22494c9170d9ebd40cce028330db09fb1
feliperx <feliipe.xaud@gmail.com>
2023-03-24 23:30:50 -0400

    added service strutucture

--- src/config/discordAuth.ts
+++ src/config/discordAuth.ts
@@ -1,8 +1,7 @@
-const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2Fcomba'; 
-const SCOPE = 'identify%20guilds%20email%20connections'; 
-const RESPONSE_TYPE='token'; 
-const CLIENT_ID = '1089017810518278145'; 
-const CDN_IMAGE = 'https://cdn.discordapp.com';
+const REDIRECT_URI = "https%3A%2F%2Fauth.expo.io%2Fcomba";
+const SCOPE = "identify%20guilds%20email%20connections";
+const RESPONSE_TYPE = "token";
+const CLIENT_ID = "1089017810518278145";
+const CDN_IMAGE = "https://cdn.discordapp.com";
 
-
-
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
