015ac19633714ad170006902c8e035a0fbbe0ba8
feliperx <feliipe.xaud@gmail.com>
2023-03-25 21:21:40 -0400

    assigned data to user: setUser (useState)

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,89 +1,89 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
 
-import * as AuthSession from 'expo-auth-session';
+import * as AuthSession from "expo-auth-session";
 import {
-    REDIRECT_URI,
-    SCOPE,
-    RESPONSE_TYPE,
-    CLIENT_ID,
-    CDN_IMAGE,
-  } from '../config/index';
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE,
+} from "../config/index";
 import { api } from "../services/api";
 
 type User = {
-    id: string;
-    usename: string;
-    firstName: string;
-    avatar: string;
-    email: string;
-    token: string;
-}
+  id: string;
+  usename: string;
+  firstName: string;
+  avatar: string;
+  email: string;
+  token: string;
+};
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
-    params: {
-        access_token: string;
-    }
-}
+  params: {
+    access_token: string;
+  };
+};
 
 type AuthContextData = {
-    user: User;
-    loading: boolean;
-    signIn: () => Promise<void>;
-}
+  user: User;
+  loading: boolean;
+  signIn: () => Promise<void>;
+};
 
 type AuthProviderProps = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 export const AuthContext = createContext({} as AuthContextData);
 
-function AuthProvider({children} : AuthProviderProps) {
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+  const [loading, setLoading] = useState(false);
 
-    const [user, setUser] = useState<User>({} as User)
-    const [loading, setLoading] = useState(false);
+  async function signIn() {
+    try {
+      setLoading(true);
 
-    async function signIn() {
-        try {
-            setLoading(true);
+      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-            const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
 
-            const {type, params} = await AuthSession
-            .startAsync({ authUrl }) as AuthorizationResponse;
+      if (type === "success") {
+        api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
-           if (type === 'success') {
-            api.defaults.headers.authorization = `Bearer ${params.access_token}`;
-           }
+        const userInfo = await api.get("/users/@me");
 
-           const userInfo = await api.get('/users/@me');
-           const firstName = userInfo.data.username.split(' ')[0];
+        const firstName = userInfo.data.username.split(" ")[0];
+        userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
 
-           setUser(userInfo);
-
-           setLoading(false);
-        } catch {
-            throw new Error("Falha ao autenticar");
-            
-        }
+        setUser({
+          ...userInfo.data,
+          firstName,
+          token: params.access_token,
+        });
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    } catch {
+      throw new Error("Falha ao autenticar");
     }
+  }
 
-    return(
-        <AuthContext.Provider
-            value={{user, loading, signIn}}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, loading, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
-    const context = useContext(AuthContext);
+  const context = useContext(AuthContext);
 
-    return context;
+  return context;
 }
 
-export {
-    AuthProvider,
-    useAuth
-}
-
+export { AuthProvider, useAuth };
