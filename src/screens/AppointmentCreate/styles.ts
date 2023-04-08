222b99e99917a1d844ddaab773c7190eebaf5bb7
feliperx <feliipe.xaud@gmail.com>
2023-04-07 23:08:49 -0400

    code cleander

--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native"; 
-import {getBottomSpace, getStatusBarHeight} from 'react-native-iphone-x-helper';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 import { theme } from "../../global/styles/theme";
 
 
