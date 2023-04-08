7d118084f5e7ec2a490d9aeacbb450dc06f38fbb
feliperx <feliipe.xaud@gmail.com>
2023-04-07 23:12:56 -0400

    added SignOut screen

---
+++ src/screens/SignOut/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+
+import { ListDivider } from "../../components/ListDivider";
+import { Load } from "../../components/Load";
+import { api } from "../../services/api";
+
+import { styles } from "./styles";
+
+
+
+
+export function SignOut() {
+
+  
+
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
